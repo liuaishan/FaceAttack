918df4335456e852bc25c7bbbd922369a4189b28
徐一涛 <2697797283@qq.com>
2019-06-16 23:07:48 +0800

    update Train code and Test code for face

--- src/train_face_model.sh
+++ src/train_face_model.sh
@@ -1,11 +1,11 @@
 python get_feature.py \
-  --load_model_path  "/media/dsg3/xuyitao/Face/model/params_res50IR_cos_CA.pkl" \
-  --load_margin_path  "/media/dsg3/xuyitao/Face/model/margin_res50IR_cos_CA.pkl" \
-  --save_model_path  "/media/dsg3/xuyitao/Face/model/params_res50IR_cos_CA.pkl"
-  --save_margin_path  "/media/dsg3/xuyitao/Face/model/margin_res50IR_cos_CA.pkl" \
-  --train_data_root "/media/dsg3/datasets/CASIA_WebFace/CASIA_align/" \
-  --train_data_list "/home/dsg/xuyitao/Face/FaceTrain/CASIA_list.txt" \
-  --train_data_fail "/home/dsg/xuyitao/Face/FaceTrain/CASIA_fail.txt" \
+  --load_model_path  "params_res50IR_cos_CA.pkl" \
+  --load_margin_path  "margin_res50IR_cos_CA.pkl" \
+  --save_model_path  "params_res50IR_cos_CA.pkl"
+  --save_margin_path  "margin_res50IR_cos_CA.pkl" \
+  --train_data_root "CASIA_WebFace/CASIA_align/" \
+  --train_data_list "CASIA_list.txt" \
+  --train_data_fail "CASIA_fail.txt" \
   --train_dataset "CASIA"
   --lr 0.05 \
   --batchsize 64 \
